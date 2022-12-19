cb69e800e96ec9b1113b88a6aa578aa088fee0a1
NicoMessina25 <nicolasmessina25@gmail.com>
2022-12-19 07:34:51 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import type {Node} from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View,Alert} from 'react-native';
+import { Colors, Header,} from 'react-native/Libraries/NewAppScreen';
+import messaging from '@react-native-firebase/messaging';
 
-export default function App() {
+let fcmUnsubscribe = null;
+
+
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.sectionContainer}>
+      <Text style={[ styles.sectionTitle, { color: isDarkMode ? Colors.white : Colors.black, }, ]}> {title} </Text>
+      <Text style={[ styles.sectionDescription, { color: isDarkMode ? Colors.light : Colors.dark,  },]}> {children} </Text>
     </View>
-  );
-}
+  ); 
+};
+
+class App extends React.Component {
+
+  componentDidMount() {
+    messaging()
+    .requestPermission()
+    .then(authStatus => {
+      console.log('APNs Status: ', authStatus);
+  
+      if (authStatus === messaging.AuthorizationStatus.AUTHORIZED || authStatus == messaging.AuthorizationStatus.PROVISIONAL) {
+        messaging()
+        .getToken()
+        .then(token => {
+          console.log('messaging.getToken: ', token);
+        });
+        messaging().onTokenRefresh(token => {
+          console.log('messaging.onTokenRefresh: ', token)
+        });
+        fcmUnsubscribe = messaging().onMessage(async remoteMessage => {
+          Alert.alert(
+              remoteMessage.notification.title, 
+              remoteMessage.notification.body)
+        });
+      }
+    })
+    .catch(err => {
+      console.log('messaging.requestPermission Error: ', err);
+    });
+  } 
+
+render () {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'light-content', 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Header />
+        <View style={{ backgroundColor: Colors.white, }}>
+          <Section title="Welcome">This is a simple <Text style={styles.highlight}> App Interface</Text> to test our Push notification App. 
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>); 
+  }; 
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
   },
 });
+
+export default App;
--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import React from 'react';
+import messaging from "@react-native-firebase/messaging";
+import { AppRegistry } from 'react-native';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message is handled in the background!', remoteMessage);
+});
+
+function HeadlessCheck({ isHeadless }) {
+  if (isHeadless) {
+    console.log('App launched by iOS in background, ignore it' );
+    // App has been launched in the background by iOS, ignore
+    return null;
+  }
+  return <App />;
+}
+
+AppRegistry.registerComponent("rnativeapp", () => HeadlessCheck)
